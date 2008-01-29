9604c2a4c27218dacea2de0c18752aab13ef8f41
Simon Aittamaa <simait-2@sigma.sm.ltu.se>
2008-01-29 19:56:18 +0100

    Started the work of implementing the same pseudo interrupts that are avaialble in the regular POSIX environment. Small fixes and made a few functions static inline for some improvements. Nothing major.

--- env/posix_srp/env.c
+++ env/posix_srp/env.c
@@ -34,37 +34,50 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
+#include <env.h>
 #include <kernel_srp.h>
-#include <posix_srp/env.h>
 
-static timer_t timer;
+/* ************************************************************************** */
+
+/** \cond */
 
 static sigset_t sigmask_protect;
 static sigset_t sigmask_unprotect;
-
-static struct timespec timestamp;
-
 static int protected;
+static pthread_t root;
+
+/*
+ * Sort of private internal but used in the header file.
+ */
+env_time_t posix_srp_timer_timestamp;
+timer_t posix_srp_timer;
+
+/* ************************************************************************** */
 
 static void interrupt_handler(int sig)
 {
 	switch (sig) {
 		case SIGALRM:
-			tt_expired(posix_srp_timer_get());
-			tt_schedule();
+			if (tt_expired(posix_srp_timer_get()))
+				tt_schedule();
 			break;
 	
 		default:
-			fprintf(stderr, "Unknown signal caught (%d).\n", sig);
+			posix_srp_panic("Unknown singal caught.\n");
 			break;
 	}
 }
 
+/** \endcond */
+
+/* ************************************************************************** */
+
 void posix_srp_init(void)
 {
 	struct sigevent timer_event;
-	memset(&timer_event, 0, sizeof(timer_event));
+	struct sigaction signal_action;
 
 	sigfillset(&sigmask_protect);
 	sigdelset(&sigmask_protect, SIGINT);
@@ -75,58 +88,54 @@ void posix_srp_init(void)
 	sigdelset(&sigmask_unprotect, SIGUSR1);
 	
 	protected = 1;
-	sigprocmask(SIG_SETMASK, &sigmask_protect, NULL);
+	pthread_sigmask(SIG_SETMASK, &sigmask_protect, NULL);
 
-	signal(SIGALRM, interrupt_handler);
-	signal(SIGUSR1, interrupt_handler);
+	memset(&signal_action, 0, sizeof(signal_action));
+	signal_action.sa_handler = interrupt_handler;
+	sigemptyset(&signal_action.sa_mask);
+	sigaction(SIGALRM, &signal_action, NULL);
+	sigaction(SIGUSR1, &signal_action, NULL);
 
+	memset(&timer_event, 0, sizeof(timer_event));
 	timer_event.sigev_notify = SIGEV_SIGNAL;
 	timer_event.sigev_signo = SIGALRM;
 
-	timer_create(CLOCK_REALTIME, &timer_event, &timer);
+	timer_create(CLOCK_REALTIME, &timer_event, &posix_srp_timer);
 
-	clock_gettime(CLOCK_REALTIME, &timestamp);
+	clock_gettime(CLOCK_REALTIME, &posix_srp_timer_timestamp);
+
+	root = pthread_self();
 }
+
+/* ************************************************************************** */
 
 void posix_srp_panic(const char * const msg)
 {
 	fprintf(stderr, msg);
-	exit(EXIT_FAILURE);
+	abort();
 }
 
-void posix_srp_timer_set(const env_time_t *next)
-{
-	struct itimerspec tmp = {.it_value = *next};
-	timer_settime(timer, TIMER_ABSTIME, &tmp, NULL);
-}
-
-env_time_t posix_srp_timer_get()
-{
-	struct timespec tmp;
-	clock_gettime(CLOCK_REALTIME, &tmp);
-	return tmp;
-}
-
-env_time_t posix_srp_timestamp(void)
-{
-	return timestamp;
-}
+/* ************************************************************************** */
 
 void posix_srp_protect(int state)
 {
 	if (state) {
-		sigprocmask(SIG_SETMASK, &sigmask_protect, NULL);
+		pthread_sigmask(SIG_SETMASK, &sigmask_protect, NULL);
 		protected = 1;
 	} else {
 		protected = 0;
-		sigprocmask(SIG_SETMASK, &sigmask_unprotect, NULL);
+		pthread_sigmask(SIG_SETMASK, &sigmask_unprotect, NULL);
 	}
 }
+
+/* ************************************************************************** */
 
 int posix_srp_isprotected(void)
 {
 	return protected;
 }
+
+/* ************************************************************************** */
 
 void posix_srp_idle(void)
 {
--- env/posix_srp/env.h
+++ env/posix_srp/env.h
@@ -35,16 +35,16 @@
 #include <stddef.h>
 #include <limits.h>
 
-#include <types_srp.h>
+#include <types.h>
 
 /* ************************************************************************** */
 
 void posix_srp_init(void);
 void posix_srp_panic(const char * const);
-void posix_srp_timer_start(void);
-void posix_srp_timer_set(const env_time_t *);
-env_time_t posix_srp_timer_get(void);
-env_time_t posix_srp_timestamp(void);
+//void posix_srp_timer_start(void);
+//void posix_srp_timer_set(const env_time_t *);
+//env_time_t posix_srp_timer_get(void);
+//env_time_t posix_srp_timestamp(void);
 void posix_srp_protect(int);
 int posix_srp_isprotected(void);
 void posix_srp_idle(void);
@@ -53,67 +53,111 @@ void posix_srp_idle(void);
 
 #define ENV_INLINE inline
 
+/* ************************************************************************** */
+
 #define ENV_CODE_FAST
+
+/* ************************************************************************** */
 
 #define ENV_INIT() \
 	posix_srp_init()
 
+/* ************************************************************************** */
+
 #define ENV_PANIC(msg) \
 	posix_srp_panic(msg)
+
+/* ************************************************************************** */
 
 #define ENV_TIMER_START() \
 	((void)0)
 
+/* ************************************************************************** */
+
 #define ENV_TIMER_GET() \
 	posix_srp_timer_get()
+
+/* ************************************************************************** */
 
 #define ENV_TIMER_SET(time) \
 	posix_srp_timer_set(&(time))
 
+/* ************************************************************************** */
+
 #define ENV_TIMESTAMP() \
 	posix_srp_timestamp()
+
+/* ************************************************************************** */
 
 #define ENV_TIME_ADD(v0, v1) \
 	posix_srp_time_add(&v0, &v1)
 
+/* ************************************************************************** */
+
 #define ENV_TIME_LE(v0, v1) \
 	(((v0).tv_sec <= (v1).tv_sec) && ((v0).tv_nsec <= (v1).tv_nsec))
+
+/* ************************************************************************** */
 
 #define ENV_TIME_LT(v0, v1) \
 	(((v0).tv_sec < (v1).tv_sec) || (((v0).tv_sec == (v1).tv_sec) && ((v0).tv_nsec < (v1).tv_nsec)))
 
+/* ************************************************************************** */
+
 #define ENV_PROTECT(state) \
 	posix_srp_protect(state)
+
+/* ************************************************************************** */
 
 #define ENV_ISPROTECTED() \
 	posix_srp_isprotected()
 
+/* ************************************************************************** */
+
 #define ENV_RESOURCE(id) \
 	(1<<id)
+
+/* ************************************************************************** */
 
 #define ENV_RESOURCE_AQUIRE(mask, resource) \
 	do {mask |= resource;} while (0)
 
+/* ************************************************************************** */
+
 #define ENV_RESOURCE_RELEASE(mask, resource) \
 	do {mask &= ~(resource);} while (0)
+
+/* ************************************************************************** */
 
 #define ENV_RESOURCE_AVAILABLE(mask, resource) \
 	(!((mask) & (resource)))
 
+/* ************************************************************************** */
+
 #define ENV_RESOURCE_MAX \
 	(sizeof(env_resource_t)*CHAR_BIT)
+
+/* ************************************************************************** */
 
 #define ENV_IDLE() \
 	posix_srp_idle()
 
+/* ************************************************************************** */
+
 #define ENV_SEC(seconds) \
 	posix_srp_sec(seconds)
+
+/* ************************************************************************** */
 
 #define ENV_MSEC(nseconds) \
 	posix_srp_nsec(nseconds)
 
+/* ************************************************************************** */
+
 #define ENV_USEC(useconds) \
 	posix_srp_usec(seconds)
+
+/* ************************************************************************** */
 
 #define ENV_STARTUP(function) \
 int main(void) {\
@@ -123,12 +167,40 @@ int main(void) {\
 	return 0;\
 } char dummy /* Force semi-colon at end of macro. */
 
-static inline struct timespec posix_srp_time_add(
-		struct timespec *v0,
-		struct timespec *v1
+/* ************************************************************************** */
+
+static inline void posix_srp_timer_set(const env_time_t *next)
+{
+	extern timer_t posix_srp_timer;
+	struct itimerspec tmp = {.it_value = *next};
+	timer_settime(posix_srp_timer, TIMER_ABSTIME, &tmp, NULL);
+}
+
+/* ************************************************************************** */
+
+static inline env_time_t posix_srp_timer_get()
+{
+	env_time_t tmp;
+	clock_gettime(CLOCK_REALTIME, &tmp);
+	return tmp;
+}
+
+/* ************************************************************************** */
+
+static inline env_time_t posix_srp_timestamp(void)
+{
+	extern env_time_t posix_srp_timer_timestamp;
+	return posix_srp_timer_timestamp;
+}
+
+/* ************************************************************************** */
+
+static inline env_time_t posix_srp_time_add(
+		env_time_t *v0,
+		env_time_t *v1
 		)
 {
-	struct timespec tmp;
+	env_time_t tmp;
 	if ((v0->tv_nsec + v1->tv_nsec) > 1000000000UL) {
 		tmp.tv_sec = v0->tv_sec + v1->tv_sec + 1;
 		tmp.tv_nsec = v0->tv_nsec + v1->tv_nsec - 1000000000UL;
@@ -139,15 +211,19 @@ static inline struct timespec posix_srp_time_add(
 	return tmp;
 }
 
-static inline struct timespec posix_srp_sec(unsigned long seconds)
+/* ************************************************************************** */
+
+static inline env_time_t posix_srp_sec(unsigned long seconds)
 {
-	struct timespec tmp = {.tv_sec = seconds, .tv_nsec = 0};
+	env_time_t tmp = {.tv_sec = seconds, .tv_nsec = 0};
 	return tmp;
 }
 
-static inline struct timespec posix_srp_msec(unsigned long useconds)
+/* ************************************************************************** */
+
+static inline env_time_t posix_srp_msec(unsigned long useconds)
 {
-	struct timespec tmp;
+	env_time_t tmp;
 	if (useconds >= 1000UL) {
 		tmp.tv_sec = useconds / 1000UL;
 		tmp.tv_nsec = useconds % 1000UL;
@@ -158,9 +234,11 @@ static inline struct timespec posix_srp_msec(unsigned long useconds)
 	return tmp;
 }
 
-static inline struct timespec posix_srp_usec(unsigned long nseconds)
+/* ************************************************************************** */
+
+static inline env_time_t posix_srp_usec(unsigned long nseconds)
 {
-	struct timespec tmp;
+	env_time_t tmp;
 	if (nseconds >= 1000000UL) {
 		tmp.tv_sec = nseconds / 1000000UL;
 		tmp.tv_nsec = nseconds % 1000000UL;
